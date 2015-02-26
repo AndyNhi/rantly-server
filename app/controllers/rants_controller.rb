2013f5968970a729ef76f41c871bc510bfc074c9
AndyNhi <nhi.atn@gmail.com>
2015-02-26 10:01:32 -0700

    destroy and update rants

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -5,5 +5,33 @@ class RantsController < ApplicationController
     render json: @user.rants.all
   end
 
+  def show
+    @user = User.find(params[:user_id])
+    @rant = @user.rants(params[:id])
+    render json: @rant
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @rant = @user.rants.find(params[:id])
+    if @rant.update(rant_params)
+      render json: @rant
+    else
+      render json: @user.rants.all
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @rant = @user.rants.find(params[:id])
+    @rant.destroy
+    render json: @user.rants.all
+  end
+
+private
+
+  def rant_params
+    params.require(:rant).permit(:title, :body, :user_id)
+  end
 
 end
--- spec/controller/rants_controller_spec.rb
+++ spec/controller/rants_controller_spec.rb
@@ -16,4 +16,45 @@ RSpec.describe RantsController, type: :controller do
     end
   end
 
+  describe '#show' do
+    it "returns json of one user rant" do
+      user = create_user
+      rant1 = create_rant(user_id: user.id)
+      rant2 = create_rant(user_id: user.id)
+
+      get :show, user_id: user.id, id: rant1.id
+      expect(response.status).to be(200)
+      response_array = JSON.parse(response.body)["rants"]
+      expect(response_array[0]["title"]).to eq(rant1.title)
+    end
+  end
+
+  describe '#update' do
+    it "returns json of an updated user rant" do
+      user = create_user
+      rant = create_rant(user_id: user.id)
+      patch :update, user_id: user.id, id: rant.id, rant: {body: "new body"}
+      expect(response.status).to be(200)
+      response_array = JSON.parse(response.body)["rants"]
+      expect(rant.reload.body).to eq("new body")
+    end
+  end
+
+  describe '#destroy' do
+
+    it "removes json rants from user" do
+      user = create_user
+      rant = create_rant(user_id: user.id)
+
+      delete :destroy, user_id: user.id, id: rant.id
+      expect(response.status).to be(200)
+      response_array = JSON.parse(response.body)
+      expect(response_array).to eq("rants"=>[])
+    end
+
+
+
+  end
+
+
 end
